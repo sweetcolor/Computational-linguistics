ba807d932fd2fd5889ee02d82cfb335d0b044d9e
antonsavchenko94 <savchenko.inc@gmail.com>
2016-11-15 14:37:42 +0200

    add table view

---
+++ public_html/table.js
@@ -0,0 +1,42 @@
+function renderTable (wordsWithRank) {
+    var tbody = document.getElementById('tbody-rank');
+    wordsWithRank.forEach(function (wordArr) {
+        var tr = document.createElement('tr');
+        for (var i = 0; i < wordArr.length; i++) {
+            var td = document.createElement('td');
+            td.innerHTML = wordArr[i];
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    })
+}
+
+
+function sortProperties (wordsCount) {
+    var sortable = [];
+    for (var key in wordsCount)
+        if (wordsCount.hasOwnProperty(key))
+            sortable.push([key, wordsCount[key]]);
+
+    sortable.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+
+    var rank = 1;
+    return sortable.map(function (word, i, arr) {
+        var wordRank = null;
+        if (i === 0) {
+            word.push(rank);
+            return word;
+        }
+        else if (word[1] === arr[i - 1][1]) {
+            word.push(arr[i - 1][2]);
+            return word;
+        }
+        else {
+            rank++;
+            word.push(rank);
+            return word;
+        }
+    });
+}
